0be66903fc057ed7e2366855ae917e3805d888b3
empress637 <122122328+empress637@users.noreply.github.com>
2023-01-19 17:16:02 -0500

    Update starburst.js
    Signed-off-by: empress637 <122122328+empress637@users.noreply.github.com>

--- threejsGraphics2023/threejsGraphics2020/js/starburst.js
+++ threejsGraphics2023/threejsGraphics2020/js/starburst.js
@@ -10,26 +10,42 @@ let cameraControls;
 let clock = new THREE.Clock();
 
 
-function createScene() {    
-	let starburst = makeStarburst(20,);
-    let axes = new THREE.AxesHelper(10);
-    scene.add(starburst);
-    scene.add(axes);
+function createSceneA() {
+    sphereRadius = 15;
+    root = starburstsOnSphereA(400, sphereRadius, 100, 1)
+    scene.add(root);
 }
 
 
-function makeStarburst(n, innerColor, outerColor, rad=10) {
+function starburstsOnSphereA(nbrBursts, sphereRadius, maxRays, maxRad) {
+    let root = new THREE.Object3D();
+    for (let i = 0; i < nbrBursts; i++) {
+        let mesh = starburstA(maxRays, maxRad);
+        let p = getRandomPointOnSphere(sphereRadius);
+        mesh.position.set(p.x, p.y, p.z);
+        root.add(mesh);
+    }
+    return root;
+}
+
+function starburstA(maxRays, maxRad) {
+    let rad = 1;   // had been rad = 10?????
     let origin = new THREE.Vector3(0, 0, 0);
+    let innerColor = getRandomColor(0.8, 0.1, 0.8);
+    let black = new THREE.Color(0x000000);
     let geom = new THREE.Geometry();
-    for (let i = 0; i < n; i++) {
-        let dest = getRandomPointOnSphere(rad);
+    let nbrRays = getRandomInt(1, maxRays);
+    for (let i = 0; i < nbrRays; i++) {
+        let r = rad * getRandomFloat(0.1, maxRad);
+        let dest = getRandomPointOnSphere(r);
         geom.vertices.push(origin, dest);
-        geom.colors.push(innerColor, outerColor);
+        geom.colors.push(innerColor, black);
     }
     let args = {vertexColors: true, linewidth: 2};
     let mat = new THREE.LineBasicMaterial(args);
     return new THREE.Line(geom, mat, THREE.LineSegments);
 }
+
 
 function animate() {
 	window.requestAnimationFrame(animate);
