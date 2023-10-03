0138de403bedd38c1401445e9f9ccc0aa51f4909
empress637 <122122328+empress637@users.noreply.github.com>
2023-10-03 09:21:00 -0400

    Update test.py
    Signed-off-by: empress637 <122122328+empress637@users.noreply.github.com>

--- test.py
+++ test.py
@@ -1,10 +1,44 @@
+import requests
+import pandas as pd
 import plotly.express as px
-fig = px.sunburst(df_m, path=['location', 'product',
-                              'payment', 'gender', 'age'],
-                  values='freq',
-                  color='freq',
-                  color_continuous_scale='rdbu_r',
-                  width=960, height=600
-                 )
-fig.update_layout(margin = dict(t=0, l=0, r=0, b=0))
-fig.show()
+
+from io import StringIO
+
+
+STATE_DATA_URL = "https://raw.githubusercontent.com/CITF-Malaysia/citf-public/main/vaccination/vax_state.csv"
+
+
+def fetch_csv(data_url: str) -> pd.DataFrame:
+    return pd.read_csv(StringIO(requests.get(data_url).text))
+
+
+def clean(df: pd.DataFrame) -> pd.DataFrame:
+    return (
+        df.set_index(["date", "state"])
+        .loc[:, ["cumul_partial", "cumul_full", "cumul"]]
+        .rename(columns={"cumul_partial": "partially_vaxed", "cumul_full": "fully_vaxed"})
+        .sort_values(by="cumul", ascending=False)
+        .sort_index(level="date", sort_remaining=False)
+        .reset_index()
+    )
+
+
+def plot_cumulative_state(df: pd.DataFrame, outfile: str):
+    fig = px.bar(
+        state_data,
+        x="state",
+        y=["partially_vaxed", "fully_vaxed"],
+        animation_frame="date",
+        animation_group="state",
+        labels={"value": "Total vaccinated", "state": "", "variable": "Dose Type"},
+        title="Vaccination Count in Malaysia by State",
+    )
+
+    fig.write_html(outfile, include_plotlyjs='cdn')
+
+
+if __name__ == "__main__":
+    state_data = fetch_csv(STATE_DATA_URL)
+    state_data = clean(state_data)
+
+    plot_cumulative_state(state_data, "index2.html")
