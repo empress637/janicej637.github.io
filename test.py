46099f86635f07bd05ea96fc195ee0442c4e9697
empress637 <122122328+empress637@users.noreply.github.com>
2023-10-02 22:44:04 -0400

    Create test.py
    Signed-off-by: empress637 <122122328+empress637@users.noreply.github.com>

---
+++ test.py
@@ -0,0 +1,10 @@
+import plotly.express as px
+fig = px.sunburst(df_m, path=['location', 'product',
+                              'payment', 'gender', 'age'],
+                  values='freq',
+                  color='freq',
+                  color_continuous_scale='rdbu_r',
+                  width=960, height=600
+                 )
+fig.update_layout(margin = dict(t=0, l=0, r=0, b=0))
+fig.show()
